1992fa4776f38cf70d89d50ff5f08172925a2b0e
Leandro de Oliveira Custódio <leandro.oliveiracustodio02@gmail.com>
2024-11-11 21:34:11 -0300

    fix isreal name

--- mind-hunt/src/components/OurTeam/OurTeam.tsx
+++ mind-hunt/src/components/OurTeam/OurTeam.tsx
@@ -6,7 +6,7 @@ export default function OurTeam() {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const teamMembers = [
-        { name: "Ireal", role: "Game Developer", picture: "https://cdn.discordapp.com/avatars/343429989104091137/44b9272f8be30d882419f689a42b214a.webp?size=240" },
+        { name: "Isreal", role: "Game Developer", picture: "https://cdn.discordapp.com/avatars/343429989104091137/44b9272f8be30d882419f689a42b214a.webp?size=240" },
         { name: "Lelis The Man", role: "Game Developer", picture: "https://cdn.discordapp.com/avatars/579352244651294730/3961565e64d39b5e478f5abc0bff743c.webp?size=240" },
         { name: "Anna_Aragão", role: "Game Producer", picture: "https://cdn.discordapp.com/avatars/1080130032153940008/b829fb9cbbdab9847304eec7014a42bb.webp?size=240" },
         { name: "Gallinho", role: "Game Developer", picture: "https://cdn.discordapp.com/avatars/1002676311208251393/e896b0b18817bc3d96fa07589e4d0c91.webp?size=128" },
