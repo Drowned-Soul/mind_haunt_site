4ceb0e7682b4b628e586a3a3f12cdb5639d3b62f
Leandro de Oliveira Custódio <leandro.oliveiracustodio02@gmail.com>
2024-12-04 09:04:36 -0300

    ajuste path

--- mind-hunt/vite.config.ts
+++ mind-hunt/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'mind_haunt_site/mind-hunt',
+  base: 'mind_haunt_site/mind-hunt/',
   plugins: [react()],
 })
