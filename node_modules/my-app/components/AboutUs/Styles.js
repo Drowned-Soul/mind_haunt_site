19da0400bab005417a9b5f16c6310cc3614f44ff
Leandro de Oliveira Custódio <leandro.oliveiracustodio02@gmail.com>
2024-12-04 17:33:21 -0300

    ajustando pastas

---
+++ node_modules/my-app/components/AboutUs/Styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+export const Container = styled.div `
+    display: flex;
+    justify-content: space-evenly;
+    padding: 60px 0;
+`;
+export const Title = styled.h1 `
+    color: #fff;
+    font-size: 50px;
+    font-family: "Almendra SC", sans-serif;
+`;
+export const Text = styled.p `
+    color: #fff;
+    font-size: 24px;
+    font-family: "Alumni Sans", sans-serif;
+`;
+export const TitleRed = styled.span `
+    color: red;
+`;
+export const TextContainer = styled.div `
+    padding: 20px 0;
+    width: 80vw;
+`;
+export const ImageContainer = styled.div `
+    display: block;
+`;
+export const Image = styled.img `
+    width: 25vw; 
+    object-fit: cover;
+`;
